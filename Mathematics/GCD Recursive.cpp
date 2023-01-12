cb18d1c1d8e155d977765daddeacc5236a339fca
Rishabh Yadav <1998rishabh31@gmail.com>
2023-01-12 21:19:53 +0530

    Update GCD Recursive.cpp

--- Mathematics/GCD Recursive.cpp
+++ Mathematics/GCD Recursive.cpp
@@ -4,12 +4,7 @@ using namespace std;
 //iterative approach
 int gcdRecursive(int a, int b)
 {
-    if(b == 0)
-        return a;
-    else
-        return gcdRecursive(b, a%b); // this will swap the values for first function call
-    
-    
+    return b == 0 ? a : gcd(b, a % b);
 }
 
 int main()
