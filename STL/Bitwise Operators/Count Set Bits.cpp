741567162518c92979bd23f94adccab8fced8a56
Rishabh Yadav <1998rishabh31@gmail.com>
2023-02-13 10:58:34 +0530

    Update Count Set Bits.cpp

--- STL/Bitwise Operators/Count Set Bits.cpp
+++ STL/Bitwise Operators/Count Set Bits.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//Brian Cunningham Solution
+int countSetBits(int number)
+{
+    int count = 0;
+    while(number != 0)
+    {
+        //this expression will make the last set bit in number from 1 to 0. The while loop will run only O(no of set bit) times.
+        number = (number & (number-1));
+        count++;
+    }
+    
+    return count;
+}
+
 int countSetBits(int number)
 {
     int x = 1;
