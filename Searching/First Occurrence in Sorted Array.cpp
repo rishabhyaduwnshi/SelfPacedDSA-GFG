5de7a35983a8983d9858bff7a1d6be5fd33ec9e6
Rishabh Yadav <1998rishabh31@gmail.com>
2023-03-11 13:17:55 +0530

    Create First Occurrence in Sorted Array.cpp

---
+++ Searching/First Occurrence in Sorted Array.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int array[], int low, int high, int key)
+{
+    if(low > high)
+        return -1;
+        
+    int mid = (low+high)/2;
+    
+    if(array[mid] > key)
+        return binarySearch(array,low,mid-1,key);
+    else if(array[mid] < key)
+        return binarySearch(array,mid+1,high,key);
+    else
+    {
+        if(mid == 0 || array[mid-1] != array[mid])
+            return mid;
+        else
+            return binarySearch(array,low,mid-1,key);
+    }
+}
+
+
+int main()
+{
+    int array[] = {1,2,3,3,3,4,5,6,7,8,8,8,9,10};
+    int size = sizeof(array)/sizeof(int);
+    int key = 8;
+    int result = binarySearch(array,0,size-1,key);
+    
+    if(result != -1)
+        cout<<"Element is present at index "<<result;
+    else
+        cout<<"Not Present";
+}
